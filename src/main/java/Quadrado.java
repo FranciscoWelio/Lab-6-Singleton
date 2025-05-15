fdb52a2f32aa3468646bb2b6cfb40801a3c93f8b
Francisco Welio <fwelio.dev@gmail.com>
2025-05-15 19:35:46 -0300

    feat/ Quadrado e triango

--- src/main/java/Quadrado.java
+++ src/main/java/Quadrado.java
@@ -1,11 +1,64 @@
 public class Quadrado implements FiguraGeometrica{
+    double ladoA;
+    double ladoB;
+    double AlturaA;
+    double AlturaB;
+
+    public double getLadoA() {
+        return ladoA;
+    }
+
+    public void setLadoA(double ladoA) {
+        this.ladoA = ladoA;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public void setLadoB(double ladoB) {
+        this.ladoB = ladoB;
+    }
+
+    public double getAlturaA() {
+        return AlturaA;
+    }
+
+    public void setAlturaA(double alturaA) {
+        AlturaA = alturaA;
+    }
+
+    public double getAlturaB() {
+        return AlturaB;
+    }
+
+    public void setAlturaB(double alturaB) {
+        AlturaB = alturaB;
+    }
+
     @Override
     public void CriarFigura() {
-
+        System.out.println(getTipo()+ "Com lado a "+ getLadoA()+" Lado b "+ getLadoB() + " e Altura A "+getAlturaA() + " Altura b "+ getAlturaB());
     }
 
     @Override
     public String getTipo() {
-        return "";
+        String s = tipo();
+        return s;
+
+    }
+
+    public String tipo(){
+        if(getLadoA() == getLadoB() && getLadoA() == getAlturaA() && getLadoA() == getAlturaB()){
+            return "Quadrado";
+        }else if(getLadoA() == getLadoB() && getAlturaA() == getAlturaB()){
+            return "Retângulo";
+        }else if(getLadoA() < getLadoB() && getAlturaA() == getAlturaB()){
+            return "Trapézio Isósceles";
+        }else if(getLadoA() < getLadoB() && getAlturaA() < getAlturaB()) {
+            return "Trapézio Escaleno";
+        }else {
+            return "Quadrilátero Irregular";
+        }
     }
 }
--- src/main/java/Triangulo.java
+++ src/main/java/Triangulo.java
@@ -1,11 +1,51 @@
 public class Triangulo implements FiguraGeometrica{
-    @Override
-    public void CriarFigura() {
 
+    double ladoA;
+    double ladoB;
+    double ladoC;
+
+    public double getLadoA() {
+        return ladoA;
     }
 
+    public void setLadoA(double ladoA) {
+        this.ladoA = ladoA;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public void setLadoB(double ladoB) {
+        this.ladoB = ladoB;
+    }
+
+    public double getLadoC() {
+        return ladoC;
+    }
+
+    public void setLadoC(double ladoC) {
+        this.ladoC = ladoC;
+    }
+    @Override
+    public void CriarFigura() {
+        System.out.println(getTipo() + " Com Lado a "+ getLadoA() +" Lado b " +getLadoB()+ " Lado c "+getLadoC());
+    }
+    public String tipo(){
+        if(getLadoA() == getLadoB() && getLadoA() == getLadoC()){
+            return "Equilátero";
+        }else if(getLadoA() == getLadoB() && getLadoA() != getLadoC() && getLadoB() != getLadoC()){
+            return "Isósceles";
+        }else if(getLadoA() != getLadoB() && getLadoA() != getLadoC() && getLadoB() != getLadoC()){
+            return "Escaleno";
+        }else {
+            return "Não existente";
+        }
+
+    }
     @Override
     public String getTipo() {
-        return "";
+        String s = "Triângulo" + tipo();
+        return s;
     }
 }
